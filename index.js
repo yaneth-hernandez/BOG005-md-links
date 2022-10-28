2e84a98de42115a062cc66aae74fcab5c87a9d00
yaneth-hernandez <yahecaceres@gmail.com>
2022-10-28 17:04:17 -0500

    Prueba parametrización --validate

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ const { getMdFiles } = require('./getMdFiles')
 const { getLinks } = require('./getLinks')
 const { validateLinks } = require('./validateLinks')
 
-const mdLinks = (route, validate) => {
+const mdLinks = (route) => {
     return new Promise((resolve, reject) => {
         const mdFiles = getMdFiles(route)
         getLinks(mdFiles) //es un promesa
             .then((links) => {
-                if(process.argv[3] !== '--validate'){
-                    validate = false
-                    console.log('Sin validate:', links)
-                }else{
-                    validate = true
-                    validateLinks(links)
-                }
-                
-                
+                validateLinks(links)
             })
             .catch((error) => {
                 reject(console.log(error))
@@ -23,4 +15,4 @@ const mdLinks = (route, validate) => {
     })
 }
 
-mdLinks(process.argv[2], process.argv[3])
+mdLinks(process.argv[2])
